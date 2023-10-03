92d4f1109a76a8f4acb3a59807a9fdb6338448fc
Alexander Jeckmans <ajeckmans@users.noreply.github.com>
2023-10-03 22:53:57 +0200

    Update task version and compilation in GitHub Actions workflow
    Update the `update-task-version.js` script in the GitHub Actions workflow to correct the paths for the `vss-extension.json` and `task.json` files. Previously, the paths were incorrect, causing the script to fail. Now, the paths are resolved correctly using the `process.argv` array, ensuring that the correct files are read and parsed.
    
    This commit fixes the issue with the script by updating the file paths in the `update-task-version.js` script in the GitHub Actions workflow.

--- .github/workflows/update-task-version.js
+++ .github/workflows/update-task-version.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const extensionJsonPath = path.join(__dirname, 'vss-extension.json');
-const taskJsonPath = path.join(__dirname, 'tasks', 'MyTask', 'task.json');
+if (process.argv.length !== 4) {
+    console.error('Usage: node update-task-version.js <path-to-vss-extension.json> <path-to-task.json>');
+    process.exit(1);
+}
+
+const extensionJsonPath = path.resolve(process.argv[2]);
+const taskJsonPath = path.resolve(process.argv[3]);
 
 const extensionJson = JSON.parse(fs.readFileSync(extensionJsonPath, 'utf-8'));
 const taskJson = JSON.parse(fs.readFileSync(taskJsonPath, 'utf-8'));
