ca3f1215ca07682dcf7df6323415ff72fb49bc7b
Alexander Jeckmans <ajeckmans@users.noreply.github.com>
2023-10-03 22:55:35 +0200

    Update task version script modified to accept a semver argument
    The update-task-version.js script has been modified to accept a semver argument instead of the path to the vss-extension.json file. The previous usage error message has been updated accordingly. The script now takes the semver argument and updates the task.json file with the provided version. The task version update is logged to the console.

--- .github/workflows/update-task-version.js
+++ .github/workflows/update-task-version.js
@@ -2,17 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 if (process.argv.length !== 4) {
-    console.error('Usage: node update-task-version.js <path-to-vss-extension.json> <path-to-task.json>');
+    console.error('Usage: node update-task-version.js <semver> <path-to-task.json>');
     process.exit(1);
 }
 
-const extensionJsonPath = path.resolve(process.argv[2]);
+const semver = process.argv[2];
 const taskJsonPath = path.resolve(process.argv[3]);
 
-const extensionJson = JSON.parse(fs.readFileSync(extensionJsonPath, 'utf-8'));
 const taskJson = JSON.parse(fs.readFileSync(taskJsonPath, 'utf-8'));
 
-const version = extensionJson.version.split('.');
+const version = semver.split('.')
 taskJson.version = {
     Major: version[0],
     Minor: version[1],
@@ -20,4 +19,4 @@ taskJson.version = {
 };
 
 fs.writeFileSync(taskJsonPath, JSON.stringify(taskJson, null, 2), 'utf-8');
-console.log('Task version updated to:', extensionJson.version);
+console.log('Task version updated to:', semver);
