96b0a0cc7d649aaf6c683ed5b0058a1592951c8a
Alexander <2088777+ajeckmans@users.noreply.github.com>
2023-10-07 21:55:09 +0200

    6 make failing the task optional (#10)
    This list of changes refines Format Check Task configuration, enforcing and tracking code formatting standards in PRs via status checks. Added interfaces, EnvVariables and TaskParameters, to manage and structure project data were also included. Updates to the release workflow ensure that PR changes publish a pre-release version of the extension to Azure DevOps Marketplace. Conditions within the workflow verify that certain actions, like uploading the .VSIX file to the GitHub release or sharing extensions, occur based on the event type. Additional updates include correctly handling the version parameter during extension packaging and shifting the extension to a private test version.
    
    ---------
    
    Co-authored-by: Alexander Jeckmans <ajeckmans@users.noreply.github.com>

--- src/format-check/scripts/format-check.ts
+++ src/format-check/scripts/format-check.ts
@@ -2,16 +2,18 @@ import {execSync} from 'child_process';
 import * as fs from 'fs';
 import dotenv from 'dotenv';
 import * as azdev from "azure-devops-node-api";
-import * as gi from "azure-devops-node-api/interfaces/GitInterfaces"
+import * as gi from "azure-devops-node-api/interfaces/GitInterfaces";
+import {TaskParameters} from './TaskParameters';
+import {EnvVariables} from './EnvVariables';
 
 import {FormatReports} from './format-report.interface';
+import {IGitApi} from "azure-devops-node-api/GitApi";
 
 const commentPreamble = '[DotNetFormatTask][Automated]';
 
-async function main() {
-    // Loading environment variables
-    dotenv.config();
+dotenv.config();
 
+async function main() {
     // Check if it's a PR build
     const pullRequestId = process.env.SYSTEM_PULLREQUEST_PULLREQUESTID;
     if (!pullRequestId) {
@@ -20,24 +22,71 @@ async function main() {
     }
 
     // Task parameters
-    const solutionPath = process.env.INPUT_SOLUTIONPATH;
-    const includePath = process.env.INPUT_INCLUDEPATH;
-    const excludePath = process.env.INPUT_EXCLUDEPATH;
+    const taskParams = getTaskParameters();
+    const envVars = getEnvVariables();
 
-    // Ennvironment variables
-    const orgUrl = process.env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI;
-    const repoId = process.env.BUILD_REPOSITORY_ID;
-    const projectId = process.env.SYSTEM_TEAMPROJECTID;
+    // Run the format check
+    const reports = runFormatCheck(taskParams);
 
-    console.log('Solution Path:', solutionPath);
-    console.log('Organization URL:', orgUrl);
-    console.log('Repo ID:', repoId);
-    console.log('Project ID:', projectId);
-    console.log('Pull Request ID:', pullRequestId);
+    // Check the format and set PR according to the result
+    await checkFormatAndSetPR(reports, envVars, taskParams.statusCheck, taskParams.failOnFormattingErrors, taskParams.statusCheckContext);
+}
 
+
+function getTaskParameters(): TaskParameters {
+    return {
+        solutionPath: process.env.INPUT_SOLUTIONPATH,
+        includePath: process.env.INPUT_INCLUDEPATH,
+        excludePath: process.env.INPUT_EXCLUDEPATH,
+        statusCheck: process.env.INPUT_STATUSCHECK === 'true',
+        failOnFormattingErrors: process.env.INPUT_FAILONFORMATTINGERRORS === 'true',
+        statusCheckContext: {
+            name: process.env.INPUT_STATUSCHECKNAME || "Code format",
+            genre: process.env.INPUT_STATUSCHECKGENRE || "YOUR_DEFAULT_GENRE",
+        }
+    };
+}
+
+function getEnvVariables(): EnvVariables {
+    return {
+        orgUrl: process.env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI,
+        repoId: process.env.BUILD_REPOSITORY_ID,
+        projectId: process.env.SYSTEM_TEAMPROJECTID,
+        pullRequestId: process.env.SYSTEM_PULLREQUEST_PULLREQUESTID ? parseInt(process.env.SYSTEM_PULLREQUEST_PULLREQUESTID) : undefined,
+        token: process.env.SYSTEM_ACCESSTOKEN
+    };
+}
+
+function runFormatCheck(taskParams: TaskParameters) {
     const reportPath = "format-report.json";
 
-    // Validate solutionPath
+    validateSolutionPath(taskParams.solutionPath, reportPath);
+
+    try {
+        const formatCmd = formatCommandString(taskParams, reportPath);
+        console.log(`Running dotnet format command. (${formatCmd})`);
+        const dotnetFormatVersion = execSync("dotnet format --version", {encoding: 'utf8'});
+        console.log(`Using dotnet format version ${dotnetFormatVersion}`);
+
+        try {
+            execSync(formatCmd, {stdio: 'inherit'});
+        } catch (error) {
+            handleDotnetFormatError(error, reportPath);
+        }
+
+        console.log("Dotnet format command completed.");
+        return loadErrorReport(reportPath);
+    } catch (error) {
+        console.error(`Dotnet format task failed with error ${error}`);
+        process.exit(1);
+    }
+}
+
+function formatCommandString(taskParams: TaskParameters, reportPath: string) {
+    return `dotnet format ${taskParams.solutionPath} --verify-no-changes --verbosity diagnostic --report ${reportPath} ${taskParams.includePath ? `--include ${taskParams.includePath}` : ''} ${taskParams.excludePath ? `--exclude ${taskParams.excludePath}` : ''}`
+}
+
+function validateSolutionPath(solutionPath: string, reportPath: string) {
     if (!solutionPath?.trim()) {
         console.error("SolutionPath is not set.");
         process.exit(1);
@@ -51,104 +100,107 @@ async function main() {
     if (fs.existsSync(reportPath)) {
         fs.unlinkSync(reportPath);
     }
+}
 
-    try {
-        // Run dotnet format with verbosity and reporting
-        const formatCmd = `dotnet format ${solutionPath} --verify-no-changes --verbosity diagnostic --report ${reportPath} ${includePath ? `--include ${includePath}` : ''} ${excludePath ? `--exclude ${excludePath}` : ''}`;
-        console.log(`Running dotnet format command. (${formatCmd})`);
-
-        const dotnetFormatVersion = execSync("dotnet format --version", {encoding: 'utf8'});
-        console.log(`using dotnet format version ${dotnetFormatVersion}`);
-
-        try {
-            execSync(formatCmd, {stdio: 'inherit'});
-        } catch (error) {
-            // The dotnet format command can return a non-zero exit code for both fatal and non-fatal errors.
-            console.error(`Dotnet format command failed with error ${error}`);
-
-            if (!fs.existsSync(reportPath)) {
-                // Format command has failed. No report was generated.
-                console.error("No report found at reportPath.");
-                process.exit(1);
-            }
-        }
-        console.log("Dotnet format command completed.");
-
-        console.log("Fetching existing threads.");
-        const token = process.env.SYSTEM_ACCESSTOKEN;
-        const authHandler = azdev.getPersonalAccessTokenHandler(token);
-        const connection = new azdev.WebApi(orgUrl, authHandler);
-
-        const gitApi = await connection.getGitApi();
-
-        const existingThreads = await gitApi.getThreads(repoId, parseInt(pullRequestId), projectId);
-
-        console.log("Completed fetching existing threads.");
-
-        console.log("Loading error report.");
-        const reports = JSON.parse(fs.readFileSync(reportPath, 'utf8')) as FormatReports;
-
-        let activeIssuesContent = [];
-
-        for (const report of reports) {
-            for (const change of report.FileChanges) {
-                const content = `${commentPreamble} ${change.DiagnosticId}: ${change.FormatDescription} on line ${change.LineNumber}, position ${change.CharNumber}`;
-                activeIssuesContent.push(content);  // Keep track of active issues
-                const existingThread = existingThreads.find(thread => thread.comments.some(comment => comment.content === content));
-
-                const comment = <gi.Comment>{
-                    content: content,
-                    commentType: gi.CommentType.Text
-                };
-
-                if (existingThread) {
-                    console.log("Updating existing thread.");
-
-                    const thread = {
-                        status: gi.CommentThreadStatus.Active
-                    };
-                    await gitApi.updateThread(thread, repoId, parseInt(pullRequestId), existingThread.id, projectId);
-                } else {
-                    console.log("Creating new thread.");
-                    const thread = <gi.GitPullRequestCommentThread>{
-                        comments: [comment],
-                        status: gi.CommentThreadStatus.Active,
-                        threadContext: {
-                            filePath: report.FilePath.replace(`${process.env.BUILD_SOURCESDIRECTORY}`, ''),
-                            rightFileStart: {line: change.LineNumber, offset: change.CharNumber},
-                            rightFileEnd: {line: change.LineNumber, offset: change.CharNumber + 1}
-                        }
-                    };
-                    await gitApi.createThread(thread, repoId, parseInt(pullRequestId), projectId);
-                }
-            }
-        }
-
-        // Close threads for resolved issues
-        for (const existingThread of existingThreads.filter(thread => thread.comments.some(comment => comment.content.startsWith(commentPreamble)))) {
-            const threadContent = existingThread.comments[0]?.content;
-            if (!activeIssuesContent.includes(threadContent)) {
-                console.log("Closing resolved thread.");
-                const closedThread = {
-                    status: gi.CommentThreadStatus.Closed
-                };
-                await gitApi.updateThread(closedThread, repoId, parseInt(pullRequestId), existingThread.id, projectId);
-            }
-        }
-
-        // if formatting errors exist, fail the task
-        if (activeIssuesContent.length) {
-            console.log("##vso[task.complete result=Failed;]Code format is incorrect.");
-        } else {
-            // no formatting errors, task succeeds
-            console.log("##vso[task.complete result=Succeeded;]Code format is correct.");
-        }
-
-        console.log("Format check script completed.");
-    } catch (error) {
-        console.error(`Dotnet format task failed with error ${error}`);
+function handleDotnetFormatError(error: any, reportPath: string) {
+    console.error(`Dotnet format command failed with error ${error}`);
+    if (!fs.existsSync(reportPath)) {
+        // Format command has failed. No report was generated.
+        console.error("No report found at reportPath.");
         process.exit(1);
     }
 }
 
+function loadErrorReport(reportPath: string) {
+    console.log("Loading error report.");
+    return JSON.parse(fs.readFileSync(reportPath, 'utf8')) as FormatReports;
+}
+
+async function checkFormatAndSetPR(reports: FormatReports, envVars: EnvVariables, statusCheck: boolean, failOnFormattingErrors: boolean, statusCheckContext: gi.GitStatusContext) {
+    console.log("Fetching existing threads.");
+    const tfsConnection = getTFSConnection(envVars.orgUrl, envVars.token);
+    const gitApi = await tfsConnection.getGitApi();
+    const existingThreads = await gitApi.getThreads(envVars.repoId, envVars.pullRequestId, envVars.projectId);
+
+    console.log("Completed fetching existing threads.");
+
+    let activeIssuesContent: string[] = [];
+
+    for (const report of reports) {
+        for (const change of report.FileChanges) {
+            const content = `${commentPreamble} ${change.DiagnosticId}: ${change.FormatDescription} on line ${change.LineNumber}, position ${change.CharNumber}`;
+            activeIssuesContent.push(content);  // Keep track of active issues
+            const existingThread = existingThreads.find(thread => thread.comments.some(comment => comment.content === content));
+
+            const comment = <gi.Comment>{
+                content: content,
+                commentType: gi.CommentType.Text
+            };
+
+            if (existingThread) {
+                console.log("Updating existing thread.");
+
+                const thread = {
+                    status: gi.CommentThreadStatus.Active
+                };
+                await gitApi.updateThread(thread, envVars.repoId, envVars.pullRequestId, existingThread.id, envVars.projectId);
+            } else {
+                console.log("Creating new thread.");
+                const thread = <gi.GitPullRequestCommentThread>{
+                    comments: [comment],
+                    status: gi.CommentThreadStatus.Active,
+                    threadContext: {
+                        filePath: report.FilePath.replace(`${process.env.BUILD_SOURCESDIRECTORY}`, ''),
+                        rightFileStart: {line: change.LineNumber, offset: change.CharNumber},
+                        rightFileEnd: {line: change.LineNumber, offset: change.CharNumber + 1}
+                    }
+                };
+                await gitApi.createThread(thread, envVars.repoId, envVars.pullRequestId, envVars.projectId);
+            }
+        }
+    }
+
+    // Close threads for resolved issues
+    await markResolvedThreadsAsClosed(existingThreads, activeIssuesContent, gitApi, envVars);
+
+    // Set PR status and fail the task if necessary
+    await setPRStatusAndFailTask(activeIssuesContent.length > 0, statusCheck, gitApi, envVars, failOnFormattingErrors, statusCheckContext);
+}
+
+function getTFSConnection(orgUrl: string, token: string) {
+    const authHandler = azdev.getPersonalAccessTokenHandler(token);
+    return new azdev.WebApi(orgUrl, authHandler);
+}
+
+async function markResolvedThreadsAsClosed(existingThreads: gi.GitPullRequestCommentThread[], activeIssuesContent: string[], gitApi: IGitApi, envVars: EnvVariables) {
+    for (const existingThread of existingThreads.filter(thread => thread.comments.some(comment => comment.content.startsWith(commentPreamble)))) {
+        const threadContent = existingThread.comments[0]?.content;
+        if (!activeIssuesContent.includes(threadContent)) {
+            console.log("Closing resolved thread.");
+            const closedThread = {
+                status: gi.CommentThreadStatus.Closed
+            };
+            await gitApi.updateThread(closedThread, envVars.repoId, envVars.pullRequestId, existingThread.id, envVars.projectId);
+        }
+    }
+}
+
+async function setPRStatusAndFailTask(formatIssuesExist: boolean, statusCheck: boolean, gitApi: IGitApi, envVars: EnvVariables, failOnFormattingErrors: boolean, statusCheckContext: gi.GitStatusContext) {
+    if (formatIssuesExist && statusCheck) {
+        const status: gi.GitPullRequestStatus = {
+            context: statusCheckContext,
+            state: gi.GitStatusState.Failed,
+            description: "Formatting errors found"
+        };
+        await gitApi.createPullRequestStatus(status, envVars.repoId, envVars.pullRequestId);
+    }
+
+    if (formatIssuesExist && failOnFormattingErrors) {
+        console.log("##vso[task.complete result=Failed;]Code format is incorrect.");
+    } else {
+        console.log("##vso[task.complete result=Succeeded;]Code format is correct.");
+    }
+}
+
+// Call these functions in your main function and do your own error handling
 main().catch(error => console.error(error));
